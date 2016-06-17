dcfe0323d4eb5d178b293ed6dd8b47c19c958266
Carol Yao <c.y-@hotmail.com>
2016-06-16 21:27:54 -0400

    Reuploaded

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,51 @@
+class ContactsController < ApplicationController
+
+  def home
+  end
+
+  def index
+    @contacts = Contact.all
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to contacts_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update_attributes(contact_params)
+      redirect_to contact_path(@contact)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    redirect_to contacts_path # note that I can't do contact_path b/c I no longer can see contact
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email, :note)
+  end
+
+end
